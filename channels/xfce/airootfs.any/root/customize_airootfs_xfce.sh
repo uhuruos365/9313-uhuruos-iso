5ea8b1cde933a421900781b5081e11750f711517
infoengine1337 <infoengine1337@gmail.com>
2022-06-02 22:24:03 +0900

    もろもろ

--- channels/xfce/airootfs.any/root/customize_airootfs_xfce.sh
+++ channels/xfce/airootfs.any/root/customize_airootfs_xfce.sh
@@ -8,21 +8,21 @@
 #
 
 # Replace wallpaper.
-#if [[ -f /usr/share/backgrounds/xfce/xfce-stripes.png ]]; then
-#    remove /usr/share/backgrounds/xfce/xfce-stripes.png
-#    ln -s /usr/share/backgrounds/alter.png /usr/share/backgrounds/xfce/xfce-stripes.png
-#fi
-#[[ -f /usr/share/backgrounds/alter.png ]] && chmod 644 /usr/share/backgrounds/alter.png
+if [[ -f /usr/share/backgrounds/xfce/xfce-stripes.png ]]; then
+    remove /usr/share/backgrounds/xfce/xfce-stripes.png
+    ln -s /usr/share/backgrounds/pokopoko.png /usr/share/backgrounds/xfce/xfce-stripes.png
+fi
+[[ -f /usr/share/backgrounds/pokopoko.png ]] && chmod 644 /usr/share/backgrounds/pokopoko.png
 
 
 # Replace right menu
-#if [[ "${language}" = "ja" ]]; then
-#    remove "/etc/skel/.config/Thunar/uca.xml"
-#    remove "/home/${username}/.config/Thunar/uca.xml"
+if [[ "${language}" = "ja" ]]; then
+    remove "/etc/skel/.config/Thunar/uca.xml"
+    remove "/home/${username}/.config/Thunar/uca.xml"
 
-#    mv "/etc/skel/.config/Thunar/uca.xml.jp" "/etc/skel/.config/Thunar/uca.xml"
-#    mv "/home/${username}/.config/Thunar/uca.xml.jp" "/home/${username}/.config/Thunar/uca.xml"
-#else
-#    remove "/etc/skel/.config/Thunar/uca.xml.jp"
-#    remove "/home/${username}/.config/Thunar/uca.xml.jp"
-#fi
+    mv "/etc/skel/.config/Thunar/uca.xml.jp" "/etc/skel/.config/Thunar/uca.xml"
+    mv "/home/${username}/.config/Thunar/uca.xml.jp" "/home/${username}/.config/Thunar/uca.xml"
+else
+    remove "/etc/skel/.config/Thunar/uca.xml.jp"
+    remove "/home/${username}/.config/Thunar/uca.xml.jp"
+fi
