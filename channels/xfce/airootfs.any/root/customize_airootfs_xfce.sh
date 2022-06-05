16d9c049f3e2675388fc5b5db7ce50b759ffed56
infoengine1337 <infoengine1337@gmail.com>
2022-06-05 18:51:35 +0900

    mod theme config and mod whisker menu

--- channels/xfce/airootfs.any/root/customize_airootfs_xfce.sh
+++ channels/xfce/airootfs.any/root/customize_airootfs_xfce.sh
@@ -8,11 +8,11 @@
 #
 
 # Replace wallpaper.
-if [[ -f /usr/share/backgrounds/xfce/xfce-verticals.png ]]; then
-    remove /usr/share/backgrounds/xfce/xfce-verticals.png
-    ln -s /usr/share/backgrounds/arch-logo-dark/ALDark1.png /usr/share/backgrounds/xfce/xfce-verticals.png
-fi
-[[ -f /usr/share/backgrounds/arch-logo-dark/ALDark1.png ]] && chmod 644 /usr/share/backgrounds/arch-logo-dark/ALDark1.png
+#if [[ -f /usr/share/backgrounds/xfce/xfce-verticals.png ]]; then
+#    remove /usr/share/backgrounds/xfce/xfce-verticals.png
+#    ln -s /usr/share/backgrounds/arch-logo-dark/ALDark1.png /usr/share/backgrounds/xfce/xfce-verticals.png
+#fi
+#[[ -f /usr/share/backgrounds/arch-logo-dark/ALDark1.png ]] && chmod 644 /usr/share/backgrounds/arch-logo-dark/ALDark1.png
 
 
 # Replace right menu
@@ -29,5 +29,5 @@ fi
 
 # Change Theme
 
-sed -i -E 's@(^    <property name="theme" type="string" value=").*("/>$)@\1Midnight-BlueNight\2@g' "/home/${username}/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml"
-sed -i -E 's@(^    <property name="ThemeName" type="string" value=").*("/>$)@\1Midnight-BlueNight\2@g' "/home/${username}/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml"
+#sed -i -E 's@(^    <property name="theme" type="string" value=").*("/>$)@\1Midnight-BlueNight\2@g' "/home/${username}/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml"
+#sed -i -E 's@(^    <property name="ThemeName" type="string" value=").*("/>$)@\1Midnight-BlueNight\2@g' "/home/${username}/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml"
