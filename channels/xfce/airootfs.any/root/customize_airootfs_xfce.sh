239d3e6f10c52229f004d113444fa1f2acd47ef1
infoengine1337 <infoengine1337@gmail.com>
2022-06-05 10:31:40 +0900

    Fix Theme Name

--- channels/xfce/airootfs.any/root/customize_airootfs_xfce.sh
+++ channels/xfce/airootfs.any/root/customize_airootfs_xfce.sh
@@ -29,5 +29,5 @@ fi
 
 # Change Theme
 
-sed -i -E 's@(^    <property name="theme" type="string" value=").*("/>$)@\1HOGEHOGE\2@g' "/home/${username}/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml"
-sed -i -E 's@(^    <property name="ThemeName" type="string" value=").*("/>$)@\1HOGEHOGEFU\2@g' "/home/${username}/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml"
+sed -i -E 's@(^    <property name="theme" type="string" value=").*("/>$)@\1Midnight-BlueNight\2@g' "/home/${username}/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml"
+sed -i -E 's@(^    <property name="ThemeName" type="string" value=").*("/>$)@\1Midnight-BlueNight\2@g' "/home/${username}/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml"
