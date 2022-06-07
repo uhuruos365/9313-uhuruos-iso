ce847b04200503f04d328bc97c7277869ae596ee
221205024 <221205024@center.meijo-u.ac.jp>
2022-06-07 15:16:26 +0900

    add loid,yor,anya

---
+++ channels/xfce_loid/airootfs.any/root/customize_airootfs_xfce.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+#
+# Yamada Hayao
+# Twitter: @Hayao0819
+# Email  : hayao@fascode.net
+#
+# (c) 2019-2021 Fascode Network.
+#
+
+# Replace wallpaper.
+if [[ -f /usr/share/backgrounds/xfce/xfce-verticals.png ]]; then
+    remove /usr/share/backgrounds/xfce/xfce-verticals.png
+    ln -s /usr/share/backgrounds/arch-logo-dark/ALDark1.png /usr/share/backgrounds/xfce/xfce-verticals.png
+fi
+[[ -f /usr/share/backgrounds/arch-logo-dark/ALDark1.png ]] && chmod 644 /usr/share/backgrounds/arch-logo-dark/ALDark1.png
+
+
+# Replace right menu
+if [[ "${language}" = "ja" ]]; then
+    remove "/etc/skel/.config/Thunar/uca.xml"
+    remove "/home/${username}/.config/Thunar/uca.xml"
+
+    mv "/etc/skel/.config/Thunar/uca.xml.jp" "/etc/skel/.config/Thunar/uca.xml"
+    mv "/home/${username}/.config/Thunar/uca.xml.jp" "/home/${username}/.config/Thunar/uca.xml"
+else
+    remove "/etc/skel/.config/Thunar/uca.xml.jp"
+    remove "/home/${username}/.config/Thunar/uca.xml.jp"
+fi
+
+# Change Theme
+
+#sed -i -E 's@(^    <property name="theme" type="string" value=").*("/>$)@\1Midnight-BlueNight\2@g' "/home/${username}/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml"
+#sed -i -E 's@(^    <property name="ThemeName" type="string" value=").*("/>$)@\1Midnight-BlueNight\2@g' "/home/${username}/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml"
