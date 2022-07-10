73a7abbf67bbde021cd9771e0d54cdabd908a424
infoengine1337 <infoengine1337@gmail.com>
2022-07-10 17:24:55 +0900

    remove pip

--- modules/obscuring/airootfs.any/root/customize_airootfs_obscuring.sh
+++ modules/obscuring/airootfs.any/root/customize_airootfs_obscuring.sh
@@ -4,9 +4,6 @@
 # infoengine1337
 # Twitter: @infoengine1337
 
-# Enable some Services
-pip install stem pyyaml
-
 _safe_systemctl mask systemd-timesyncd.service
 _safe_systemctl enable iptables.service macspoof.service NetworkManager.service dnsmasq.service secure-time-sync.service onion-grater.service kloak.service
 _safe_systemctl mask tor.service
