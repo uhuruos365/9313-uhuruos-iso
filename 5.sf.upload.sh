37c42fc8ca0aa6eff48ba38d5192b4255e921643
gnuhub <gnuhub@gmail.com>
2022-11-04 13:53:39 +0800

    auto commit

--- 5.sf.upload.sh
+++ 5.sf.upload.sh
@@ -8,10 +8,8 @@ export PROJECT_NAME="${CMD_PATH##*/}"
 
 cd $CMD_PATH
 mkdir -p ~/.ssh/
-echo $MY_SF_SSH > ~/.ssh/id_ed25519
+echo "$MY_SF_SSH" > ~/.ssh/id_ed25519
 chmod 600 ~/.ssh/id_ed25519
 ssh-keygen -f "$HOME/.ssh/known_hosts" -R "frs.sourceforge.net"
 ssh-keyscan "frs.sourceforge.net" >> ~/.ssh/known_hosts
-
-
 rsync -avzP  ./out/  gnuhub@frs.sourceforge.net:/home/frs/project/$GITHUB_REF_NAME/$GITHUB_RUN_NUMBER/
